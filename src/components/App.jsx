57fb5227c6c53dca95fd2891109c017ea2f73800
mehmood.hassan <mehmood.hassan@emumba.com>
2023-02-11 14:03:10 +0500

    Setup styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,13 +3,16 @@ import { CssBaseline } from '@mui/material';
 import { Switch, Route } from 'react-router-dom';
 
 import { Actors, MovieInformation, Movies, Navbar, Profile } from '.';
+import useStyles from './styles';
 
 function App() {
+  const classes = useStyles();
   return (
-    <div>
+    <div className={classes.root}>
       <CssBaseline />
       <Navbar />
-      <main>
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
         <Switch>
           <Route path="/" exact component={Movies} />
           <Route path="/movies/:id" exact component={MovieInformation} />
