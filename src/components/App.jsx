dc943e1ca7f3dbcfa6a2828080239558f0b76608
mehmood.hassan <mehmood.hassan@emumba.com>
2023-02-11 13:42:08 +0500

    added navigation

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import { CssBaseline } from '@mui/material';
+import { Switch, Route } from 'react-router-dom';
+
+import { Actors, MovieInformation, Movies, Navbar, Profile } from '.';
 
 function App() {
   return (
     <div>
       <CssBaseline />
-      <h1>App</h1>
+      <Navbar />
+      <main>
+        <Switch>
+          <Route path="/" exact component={Movies} />
+          <Route path="/movies/:id" exact component={MovieInformation} />
+          <Route path="/actors/:id" exact component={Actors} />
+          <Route path="/profile" exact component={Profile} />
+        </Switch>
+      </main>
     </div>
   );
 }
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Actors } from './Actors';
+export { default as Movies } from './Movies';
+export { default as MovieInformation } from './Movies/Info';
+export { default as Profile } from './Profile';
+export { default as Navbar } from './NavBar';
