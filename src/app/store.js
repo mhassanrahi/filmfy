2c1851f0dd21907e190c63034a5fb129aef3d988
mehmood.hassan <mehmood.hassan@emumba.com>
2023-03-11 11:38:16 +0500

    * Setup RTK-Query API * Setup middleware for

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/query';
 import { tmdbApi } from '../services/tmdb';
 
 const store = configureStore({
   reducer: {
     [tmdbApi.reducerPath]: tmdbApi.reducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(tmdbApi.middleware),
 });
+
+setupListeners(store.dispatch);
 
 export default store;
