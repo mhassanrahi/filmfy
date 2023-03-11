3a0483bba7bcf6da14a59598483d25e1d307f8cb
mehmood.hassan <mehmood.hassan@emumba.com>
2023-03-11 10:28:56 +0500

    * Added redux service to fetch movies

--- src/components/Movies/index.jsx
+++ src/components/Movies/index.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
+import { useGetMoviesQuery } from '../../services/tmdb';
 
 function Movies() {
+  const { data } = useGetMoviesQuery();
+  console.log('data :', data);
+
   return (
     <div>Movies</div>
   );
---
+++ src/services/tmdb.js
@@ -0,0 +1,21 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const apiKey = process.env.REACT_APP_TMDB_KEY;
+const page = 1;
+
+export const tmdbApi = createApi({
+  reducerPath: 'tmdbApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://api.themoviedb.org/3',
+  }),
+  endpoints: (builder) => ({
+    // Get movies by type
+    getMovies: builder.query({
+      query: () => `movie/popular?api_key=${apiKey}&page=${page}`,
+    }),
+  }),
+});
+
+export const {
+  useGetMoviesQuery,
+} = tmdbApi;
