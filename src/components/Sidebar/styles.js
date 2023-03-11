a69aae7f64c316ef2b88f8b4e6163d50ef7a000b
mehmood.hassan <mehmood.hassan@emumba.com>
2023-03-11 08:29:26 +0500

    * Added styles for sidebar

---
+++ src/components/Sidebar/styles.js
@@ -0,0 +1,22 @@
+import { makeStyles } from '@mui/styles';
+
+export default makeStyles((theme) => ({
+  imageLink: {
+    display: 'flex',
+    justifyContent: 'center',
+    padding: '5% 0',
+  },
+
+  image: {
+    width: '60%',
+  },
+
+  links: {
+    color: theme.palette.text.primary,
+    textDecoration: 'none',
+  },
+
+  genreImage: {
+    filter: theme.palette.mode === 'dark' ? 'dark' : 'invert(1)',
+  },
+}));
