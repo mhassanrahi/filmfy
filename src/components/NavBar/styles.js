1ab4ad95d6e0a645ea71bc765eb719bcbfac280b
mehmood.hassan <mehmood.hassan@emumba.com>
2023-03-11 08:29:49 +0500

    * Updated navigation styles

--- src/components/NavBar/styles.js
+++ src/components/NavBar/styles.js
@@ -1,5 +1,7 @@
 import { makeStyles } from '@mui/styles';
 
+const drawerWidth = 240;
+
 export default makeStyles((theme) => ({
   toolbar: {
     height: '80px',
@@ -8,6 +10,7 @@ export default makeStyles((theme) => ({
     marginLeft: '240px',
     [theme.breakpoints.down('sm')]: {
       marginLeft: 0,
+      flexWrap: 'wrap',
     },
   },
 
@@ -15,6 +18,24 @@ export default makeStyles((theme) => ({
     marginRight: theme.spacing(2),
     [theme.breakpoints.up('sm')]: {
       display: 'none',
+    },
+  },
+
+  drawer: {
+    [theme.breakpoints.up('sm')]: {
+      width: drawerWidth,
+      flexShrink: 0,
+    },
+  },
+
+  drawerPaper: {
+    width: drawerWidth,
+  },
+
+  linkButton: {
+    '&:hover': {
+      color: 'white !important',
+      textDecoration: 'none',
     },
   },
 }));
