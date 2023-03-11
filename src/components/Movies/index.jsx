2e55c3ceabf168b70ec28d80247ff27f7f1e2330
mehmood.hassan <mehmood.hassan@emumba.com>
2023-03-11 11:39:37 +0500

    * Added loader and fetched the movies

--- src/components/Movies/index.jsx
+++ src/components/Movies/index.jsx
@@ -1,12 +1,38 @@
 import React from 'react';
+import { Box, CircularProgress, Typography } from '@mui/material';
+import MovieList from './MovieList';
 import { useGetMoviesQuery } from '../../services/tmdb';
 
 function Movies() {
-  const { data } = useGetMoviesQuery();
-  console.log('data :', data);
+  const { data, error, isFetching } = useGetMoviesQuery();
+  if (isFetching) {
+    return (
+      <Box display="flex" justifyContent="center">
+        <CircularProgress size="4rem" />
+      </Box>
+    );
+  }
+
+  if (!data.results.length) {
+    return (
+      <Box display="flex">
+        <Typography variant="h4">
+          No movies found.
+          <br />
+          Please search for something else.
+        </Typography>
+      </Box>
+    );
+  }
+
+  if (error) {
+    return 'Something went wrong. Please try again.';
+  }
 
   return (
-    <div>Movies</div>
+    <div>
+      <MovieList movies={data?.results} />
+    </div>
   );
 }
 
