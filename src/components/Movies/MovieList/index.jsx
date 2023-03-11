5dc66353a85a417d7237e000ff63c2dc10851ccc
mehmood.hassan <mehmood.hassan@emumba.com>
2023-03-11 11:38:39 +0500

    * Added MovieList component

---
+++ src/components/Movies/MovieList/index.jsx
@@ -0,0 +1,19 @@
+import { Grid } from '@mui/material';
+import React from 'react';
+import Movie from '../Movie';
+import useStyles from './styles';
+
+function MovieList({ movies }) {
+  const classes = useStyles();
+  return (
+    <Grid container className={classes.movieContainer}>
+      {
+        movies.map((movie, index) => (
+          <Movie key={index} movie={movie} index={index} />
+        ))
+      }
+    </Grid>
+  );
+}
+
+export default MovieList;
--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,5 @@ export { default as MovieInformation } from './Movies/Info';
 export { default as Profile } from './Profile';
 export { default as Navbar } from './NavBar';
 export { default as Sidebar } from './Sidebar';
+export { default as MovieList } from './Movies/MovieList';
+export { default as Movie } from './Movies/Movie';
