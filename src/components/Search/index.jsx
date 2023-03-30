a7e23251f1327d24e198fa25b8914efb352c92fc
mehmood.hassan <mehmood.hassan@emumba.com>
2023-03-31 01:34:27 +0500

    * Show search bar on homepage only

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -2,6 +2,7 @@ import { Search as SearchIcon } from '@mui/icons-material';
 import { InputAdornment, TextField } from '@mui/material';
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
+import { useLocation } from 'react-router-dom';
 import { searchMovie } from '../../features/currentGenreOrCategory';
 
 import useStyles from './styles';
@@ -10,12 +11,15 @@ function Search() {
   const [query, setQuery] = useState('');
   const classes = useStyles();
   const dispatch = useDispatch();
+  const location = useLocation();
 
   const handleSearch = (event) => {
     if (event.key === 'Enter') {
       dispatch(searchMovie(query));
     }
   };
+
+  if (location.pathname !== '/') return null;
   return (
     <div className={classes.container}>
       <TextField
